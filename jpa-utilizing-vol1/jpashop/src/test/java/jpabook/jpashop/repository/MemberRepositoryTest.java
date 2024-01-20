b7b835fc2f6f42942e43a28f82ce52817a833344
Hambak-note <dudghks5722@gmail.com>
2024-01-20 19:23:05 +0900

    feat: 실습에 사용할 도메인 구현

--- jpa-utilizing-vol1/jpashop/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ jpa-utilizing-vol1/jpashop/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -2,7 +2,6 @@ package jpabook.jpashop.repository;
 
 
 import jpabook.jpashop.domain.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +9,6 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
